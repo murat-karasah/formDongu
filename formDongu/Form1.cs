9243118d83f565bb377dae6e6d84e5595a30499c
Murat Karasah <mkarasah@gmail.com>
2021-12-01 14:16:25 +0300

    Add project files.

---
+++ formDongu/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace formDongu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            int i = 1;
+            for (; ; )
+            {
+                MessageBox.Show(i + ".Mesaj");
+                i++;
+                if (i > 5)
+                {
+                    break;
+                }
+            }
+        }
+
+
+    }
+}
